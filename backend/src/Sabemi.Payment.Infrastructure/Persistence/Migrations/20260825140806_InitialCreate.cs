1d515e342d640c407fdff890507a75d9af41daae
Gabriel Breder <gabrielsousabreder33@gmail.com>
2026-08-25 13:48:15 -0300

    fix: log

--- backend/src/Sabemi.Payment.Infrastructure/Persistence/Migrations/20260825140806_InitialCreate.cs
+++ backend/src/Sabemi.Payment.Infrastructure/Persistence/Migrations/20260825140806_InitialCreate.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Sabemi.Payment.Infrastructure.Persistence.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -56,7 +54,6 @@ namespace Sabemi.Payment.Infrastructure.Persistence.Migrations
                 unique: true);
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
